8b5f9f545fa1ebe006cf3342fc6cdab6a0f2518f
Camille <ferreiracamille819@gmail.com>
2025-09-01 17:34:00 -0300

    Add files via upload

---
+++ Exercicio03/src/exercicio03/Exercicio03.java
@@ -0,0 +1,10 @@
+package exercicio03;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+         Exercicio3 ex3 = new Exercicio3("Olá,", " mundo!");
+        String resultado = ex3.concatenar();
+        System.out.println(resultado);
+    }
+}
---
+++ Exercicio03/src/exercicio03/Exercicio3.java
@@ -0,0 +1,17 @@
+package exercicio03;
+
+public class Exercicio3 {
+
+    private final String parte1;
+    private final String parte2;
+
+    public Exercicio3(String parte1, String parte2) {
+        this.parte1 = parte1;
+        this.parte2 = parte2;
+    }
+
+    public String concatenar() {
+        return parte1 + parte2;
+    }
+
+}
