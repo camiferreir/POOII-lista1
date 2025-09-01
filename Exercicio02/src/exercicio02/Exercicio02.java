fbbf357b4bbcbd9ca6f839042004695cec0d626a
Camille <ferreiracamille819@gmail.com>
2025-09-01 17:45:37 -0300

    Add files via upload

---
+++ Exercicio02/src/exercicio02/Exercicio02.java
@@ -0,0 +1,15 @@
+package exercicio02;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nomeDigitado = scanner.nextLine();
+
+        scanner.close();
+    }
+}
---
+++ Exercicio02/src/exercicio02/Exercicio2.java
@@ -0,0 +1,15 @@
+package exercicio02;
+
+public class Exercicio2 {
+
+    public Exercicio2(String nomeDigitado) {
+
+    }
+
+    public void saudar() {
+  String nome = null;
+
+        System.out.println("Olá, " + nome + "! Bem-vindo ao programa.");
+    }
+
+}
