1742d9867da87bd1cf030b3c614d5ee02ce8599a
Camille <ferreiracamille819@gmail.com>
2025-09-01 16:44:38 -0300

    Add files via upload

---
+++ Exercicio01/src/exercicio01/Exercicio01.java
@@ -0,0 +1,11 @@
+package exercicio01;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        Exercicio1 ex1 = new Exercicio1("Olá, mundo");
+        System.out.println("Olá, mundo");
+        ex1.imprimirMensagem();
+    }
+
+}
---
+++ Exercicio01/src/exercicio01/Exercicio1.java
@@ -0,0 +1,13 @@
+package exercicio01;
+
+public final class Exercicio1 {
+
+    public Exercicio1(String mensagem) {
+        this.imprimirMensagem();
+    }
+
+    public void imprimirMensagem() {
+     
+    }
+
+}
